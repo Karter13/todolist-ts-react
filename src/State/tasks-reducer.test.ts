90f2684f93461023c80ede3f273f38ef04dbef65
Karter13 <KarMiH1975@mail.ru>
2020-08-26 22:41:30 +0300

    fixed tests in tasks-reducer and todolists-reducer

--- src/State/tasks-reducer.test.ts
+++ src/State/tasks-reducer.test.ts
@@ -3,7 +3,6 @@ import {TasksStateType} from '../App';
 import {addTodolistAC, removeTodolistAC} from './todolists-reducer';
 
 let  startState: TasksStateType;
-
 beforeEach(() => {
     startState = {
         "todolistId1": [
@@ -20,18 +19,6 @@ beforeEach(() => {
 });
 
 test('correct task should be deleted from correct array', () => {
-    // const startState: TasksStateType = {
-    //     "todolistId1": [
-    //         { id: "1", title: "CSS", isDone: false },
-    //         { id: "2", title: "JS", isDone: true },
-    //         { id: "3", title: "React", isDone: false }
-    //     ],
-    //     "todolistId2": [
-    //         { id: "1", title: "bread", isDone: false },
-    //         { id: "2", title: "milk", isDone: true },
-    //         { id: "3", title: "tea", isDone: false }
-    //     ]
-    // };
 
     const action = removeTaskAC("2", "todolistId2");
 
@@ -43,18 +30,6 @@ test('correct task should be deleted from correct array', () => {
 });
 
 test('correct task should be added to correct array', () => {
-    // const startState: TasksStateType = {
-    //     "todolistId1": [
-    //         { id: "1", title: "CSS", isDone: false },
-    //         { id: "2", title: "JS", isDone: true },
-    //         { id: "3", title: "React", isDone: false }
-    //     ],
-    //     "todolistId2": [
-    //         { id: "1", title: "bread", isDone: false },
-    //         { id: "2", title: "milk", isDone: true },
-    //         { id: "3", title: "tea", isDone: false }
-    //     ]
-    // };
 
     const action = addTasksAC("juce", "todolistId2");
     const endState = tasksReducer(startState, action)
@@ -67,18 +42,6 @@ test('correct task should be added to correct array', () => {
 });
 
 test('status of specified task should be changed', () => {
-    // const startState: TasksStateType = {
-    //     "todolistId1": [
-    //         { id: "1", title: "CSS", isDone: false },
-    //         { id: "2", title: "JS", isDone: true },
-    //         { id: "3", title: "React", isDone: false }
-    //     ],
-    //     "todolistId2": [
-    //         { id: "1", title: "bread", isDone: false },
-    //         { id: "2", title: "milk", isDone: true },
-    //         { id: "3", title: "tea", isDone: false }
-    //     ]
-    // };
 
     const action = changeTaskStatusAC("2", false, "todolistId2");
 
@@ -89,18 +52,6 @@ test('status of specified task should be changed', () => {
 });
 
 test('title of task should be changed', () => {
-    // const startState: TasksStateType = {
-    //     "todolistId1": [
-    //         { id: "1", title: "CSS", isDone: false },
-    //         { id: "2", title: "JS", isDone: true },
-    //         { id: "3", title: "React", isDone: false }
-    //     ],
-    //     "todolistId2": [
-    //         { id: "1", title: "bread", isDone: false },
-    //         { id: "2", title: "milk", isDone: true },
-    //         { id: "3", title: "tea", isDone: false }
-    //     ]
-    // };
 
     const action = changeTaskTitleAC("2", 'BEER', "todolistId2");
 
@@ -111,18 +62,6 @@ test('title of task should be changed', () => {
 });
 
 test('new property with new array should be added when new todolist is added', () => {
-    // const startState: TasksStateType = {
-    //     "todolistId1": [
-    //         { id: "1", title: "CSS", isDone: false },
-    //         { id: "2", title: "JS", isDone: true },
-    //         { id: "3", title: "React", isDone: false }
-    //     ],
-    //     "todolistId2": [
-    //         { id: "1", title: "bread", isDone: false },
-    //         { id: "2", title: "milk", isDone: true },
-    //         { id: "3", title: "tea", isDone: false }
-    //     ]
-    // };
 
     const action = addTodolistAC("new todolist");
 
@@ -140,18 +79,6 @@ test('new property with new array should be added when new todolist is added', (
 });
 
 test('property with todolistId should be deleted', () => {
-    // const startState: TasksStateType = {
-    //     "todolistId1": [
-    //         { id: "1", title: "CSS", isDone: false },
-    //         { id: "2", title: "JS", isDone: true },
-    //         { id: "3", title: "React", isDone: false }
-    //     ],
-    //     "todolistId2": [
-    //         { id: "1", title: "bread", isDone: false },
-    //         { id: "2", title: "milk", isDone: true },
-    //         { id: "3", title: "tea", isDone: false }
-    //     ]
-    // };
 
     const action = removeTodolistAC("todolistId2");
 
--- src/State/todolists-reducer.test.ts
+++ src/State/todolists-reducer.test.ts
@@ -11,7 +11,6 @@ import {FilterValuesType, TodoListType} from '../App';
 let todolistId1: string;
 let todolistId2: string;
 let startState: Array<TodoListType>;
-
 beforeEach(() => {
     todolistId1 = v1();
     todolistId2 = v1();
@@ -22,15 +21,7 @@ beforeEach(() => {
 });
 
 
-
 test('correct todolist should be removed', () => {
-    // let todolistId1 = v1();
-    // let todolistId2 = v1();
-    //
-    // const startState: Array<TodoListType> = [
-    //     {id: todolistId1, title: "What to learn", filter: "all"},
-    //     {id: todolistId2, title: "What to buy", filter: "all"}
-    // ];
 
     const endState = todolistsReducer(startState, removeTodolistAC(todolistId1));
 
@@ -39,49 +30,28 @@ test('correct todolist should be removed', () => {
 });
 
 test('correct todolist should be added', () => {
-    // let todolistId1 = v1();
-    // let todolistId2 = v1();
 
     let newTodolistTitle = "New Todolist";
-
-    // const startState: Array<TodoListType> = [
-    //     {id: todolistId1, title: "What to learn", filter: "all"},
-    //     {id: todolistId2, title: "What to buy", filter: "all"}
-    // ];
 
     const endState = todolistsReducer(startState, addTodolistAC(newTodolistTitle));
 
     expect(endState.length).toBe(3);
-    expect(endState[2].title).toBe(newTodolistTitle);
+    expect(endState[0].title).toBe(newTodolistTitle);
 });
 
 test('correct todolist should change its name', () => {
-    // let todolistId1 = v1();
-    // let todolistId2 = v1();
 
     let newTodolistTitle = "New Todolist";
 
-    // const startState: Array<TodoListType> = [
-    //     {id: todolistId1, title: "What to learn", filter: "all"},
-    //     {id: todolistId2, title: "What to buy", filter: "all"}
-    // ];
-
-    const endState = todolistsReducer(startState, changeTodolistTitleAC(todolistId2, newTodolistTitle));
+   const endState = todolistsReducer(startState, changeTodolistTitleAC(todolistId2, newTodolistTitle));
 
     expect(endState[0].title).toBe("What to learn");
     expect(endState[1].title).toBe(newTodolistTitle);
 });
 
 test('correct filter of todolist should be changed', () => {
-    // let todolistId1 = v1();
-    // let todolistId2 = v1();
 
     let newFilter: FilterValuesType = "completed";
-
-    // const startState: Array<TodoListType> = [
-    //     {id: todolistId1, title: "What to learn", filter: "all"},
-    //     {id: todolistId2, title: "What to buy", filter: "all"}
-    // ];
 
     const endState = todolistsReducer(startState, changeTodolistFilterAC(todolistId2, newFilter));
 
