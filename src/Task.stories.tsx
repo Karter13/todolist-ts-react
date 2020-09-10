5a11ad7292e273731477f33d64c36dad839be035
Karter13 <karmih1975@mail.ru>
2020-09-10 15:00:42 +0300

    created Task.stories.tsx

---
+++ src/Task.stories.tsx
@@ -0,0 +1,32 @@
+import {action} from '@storybook/addon-actions';
+import React from 'react';
+import {Task} from './Task';
+
+export default {
+    title: 'AddItemForm component',
+    component: Task
+}
+
+const changeTaskStatusCallback = action('Status changed');
+const changeTaskTitleCallback = action('Title changed');
+const removeTaskCallback = action('Task removed');
+
+export const TaskBaseExample = (props: any) => {
+    return <>
+        <Task
+            task={{id: '1', isDone: true, title: 'CSS' }}
+            todolistId={'todolistId1'}
+            removeTask={removeTaskCallback}
+            changeTaskTitle={changeTaskTitleCallback}
+            changeTaskStatus={changeTaskStatusCallback}
+        />
+        <Task
+            task={{id: '2', isDone: false, title: 'React' }}
+            todolistId={'todolistId2'}
+            removeTask={removeTaskCallback}
+            changeTaskTitle={changeTaskTitleCallback}
+            changeTaskStatus={changeTaskStatusCallback}
+        />
+        </>
+
+};
