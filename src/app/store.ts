18c042a1b0a4735a86a165c4ccba197775b6450f
Karter13 <karmih1975@mail.ru>
2020-10-23 13:24:44 +0300

    created store with configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {combineReducers} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import {appReducer} from './app-reducer';
 import {authReducer} from '../features/Login/auth-reduce';
+import {configureStore} from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
@@ -12,7 +13,15 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+//with applyMiddleware without @reduxjs/toolkit
+// export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+});
+
 
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 
