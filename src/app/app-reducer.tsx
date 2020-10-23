0b401ac958fc1a54fa634c578124aae88efa7c9c
Karter13 <karmih1975@mail.ru>
2020-10-23 13:58:03 +0300

    fixed auth-reducer

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -30,7 +30,7 @@ export const setAppIsInitializedAC = (isInitialized: boolean) => ({type: 'APP/SE
 export const initializeAppTC = () => (dispatch: Dispatch) => {
     authAPI.me().then(res => {
         if (res.data.resultCode === 0) {
-            dispatch(setIsLoggedInAC(true));
+            dispatch(setIsLoggedInAC({value: true}));
         } else {
         }
         dispatch(setAppIsInitializedAC(true))
